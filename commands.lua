4ecc5e8a8adab6e2a85f4acd8caa8a419e3de64c
Michal Svorc <dev@michalsvorc.com>
2023-11-07 18:51:21 +0100

    feat(git): initial commit

---
+++ commands.lua
@@ -0,0 +1,9 @@
+return {
+  browser = 'firefox',
+  browser_development = 'firefox-developer',
+  browser_private ='firefox --private-window',
+  code='code',
+  display='arandr',
+  screenshot='import tmp/screenshot.png',
+  terminal = 'alacritty',
+ }
